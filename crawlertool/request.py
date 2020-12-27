5732967012f599f066fb492f1bee79d7f5cb2274
ChangxingJiang <1278729001@qq.com>
2020-12-27 15:45:52 +0800

    release V0.1.15

--- crawlertool/request.py
+++ crawlertool/request.py
@@ -3,20 +3,24 @@ import time
 import requests
 
 
-def do_request(url, params=None, headers=None, proxies=None, timeout=3, verify=True):
+def do_request(url, params=None, **kwargs):
     """执行HTTP请求：如果请求成功则返回请求结果；如果请求失败则返回None"""
+
+    # 将默认的请求时间设置为3秒
+    if "timeout" not in kwargs:
+        kwargs["timeout"] = 3
+
     try:
-        if response := requests.get(url=url, params=params, headers=headers, proxies=proxies,
-                                    timeout=timeout, verify=verify):
+        if response := requests.get(url=url, params=params, **kwargs):
             return response.content.decode(encoding="UTF-8", errors="ignore")
     except requests.RequestException:
         return None
 
 
-def try_request(url, headers=None, proxies=None, times=3, interval=10, timeout=3):
+def try_request(url, params=None, times=3, interval=10, **kwargs):
     """尝试执行HTTP请求：尝试请求times次，每次请求之间间隔interval秒，如果最终请求失败则返回None"""
     for _ in range(times):
-        if response := do_request(url=url, headers=headers, proxies=proxies, timeout=timeout):
+        if response := do_request(url=url, params=params, **kwargs):
             return response
         else:
             time.sleep(interval)
