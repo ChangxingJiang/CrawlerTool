dda9c0206c3b1bd56b011d4a1185dc0c8616f216
ChangxingJiang <1278729001@qq.com>
2021-01-04 18:39:02 +0800

    release V0.1.24

--- crawlertool/io/csv.py
+++ crawlertool/io/csv.py
@@ -6,4 +6,4 @@ import pandas as pd
 def write_csv(path: str, columns: List[str], data: List[Dict]):
     """将数据写入到csv文件"""
     df = pd.DataFrame([pd.Series(item) for item in data], columns=columns)
-    df.to_csv(path)
+    df.to_csv(path, mode="a+")
